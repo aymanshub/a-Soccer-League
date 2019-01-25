2e253311ea56dff4f50b1e90835f576de7642121
Ayman Said <ayman-said@live.com>
2019-01-25 14:38:08 +0200

    intial documentation

---
+++ league_builder.py
@@ -0,0 +1,32 @@
+"""
+Python Web Development Techdegree
+Project 2 - A Soccer League
+--------------------------------
+Developed by: Ayman Said
+Jan-2019
+"""
+
+import csv
+
+INPUT_FILE_NAME = "soccer_players.csv"
+
+
+def read_input_file(file_name):
+    """
+    Reads the given csv file_name and stores the data into data structure.
+    :param file_name: the file name that should be read
+    :return: None
+    """
+    with(INPUT_FILE_NAME) as csvfile:
+        filednames =
+    pass
+
+
+if __name__ == "__main__":
+
+    # Read ​& store the ​supplied ​CSV ​file into data structures. could be a function call,
+    # analyze input validity to goal (if the number is dividable)
+    # divide and output to teams.txt file
+
+    read_input_file(INPUT_FILE_NAME)
+
